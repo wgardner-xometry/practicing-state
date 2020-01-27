906f0daa1581b8474ab755b9a2600028265dfdee
wagx92 <wagx92@gmail.com>
2020-01-26 23:44:25 -0500

    Making rating system work.

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,24 +1,45 @@
 import React, { Component } from "react";
-import Star from './Star';
+import Star from "./Star";
 
 class StarRating extends Component {
-
   // Initialize a 'rating' state
-
+  state = {
+    rating: 0,
+  };
 
   // Write a function that returns 5 Star components
-
+  renderStars = () => {
+    let stars = [];
+    let maxRating = 5;
+    for (let i = 0; i < maxRating; i++) {
+      stars.push(
+        <Star
+          key={i}
+          isSelected={this.state.rating > i}
+          setRating={() => this.setRating(i + 1)}
+        />,
+      );
+    }
+    return stars;
+  };
 
   // Write an event handler that updates the rating state.
+  setRating = rating => {
+    if (rating === this.state.rating) {
+      this.setState({
+        rating: 0,
+      });
+    } else {
+      this.setState({
+        rating,
+      });
+    }
+  };
+
   // Pass the function to a Star component via props
 
-
   render() {
-    return (
-      <ul className="course--stars">
-        {/* Render the Star components */}
-      </ul>
-    );
+    return <ul className="course--stars">{this.renderStars()}</ul>;
   }
 }
 
